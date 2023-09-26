70a66b3440a6f376276aef0df47859d3c7ce7539
Taniya Anjalie <anjalietaniya@gmail.com>
2023-09-27 05:26:21 +0530

    Event bakend

--- backend/Server.js
+++ backend/Server.js
@@ -1,40 +1,75 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// require('dotenv').config();
+
+// const app = express();
+
+// const ArtRoute = require('./Routes/ArtRoute')
+
+// app.use(express.json());
+
+// app.use('/art', ArtRoute)
+
+// const MONGO_URL = process.env.MONGO_URL;
+// const PORT = process.env.PORT || 3000;
+
+// // Connect to MongoDB
+// mongoose.connect('MONGO_URL', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//   .then(() => {
+//     console.log('Connected to MongoDB!');
+
+//     // Start the Express server
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//       console.log(`Node API is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
+
+// // app.post('/art', (req, res) => {
+// //   console.log(req.body);
+// //   res.json({ message: 'Data received successfully' });
+// // });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello');
+// });
+
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
-
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-const ArtRoute = require('./Routes/ArtRoute')
-
-app.use(express.json());
-
-app.use('/art', ArtRoute)
-
-const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('MONGO_URL', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://admin:artworkAdmin@artwork.f0skof2.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  .then(() => {
-    console.log('Connected to MongoDB!');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-    // Start the Express server
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Node API is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+// Routes
+const eventRoutes = require('./Routes/eventRoutes');
+app.use('/events', eventRoutes);
 
-// app.post('/art', (req, res) => {
-//   console.log(req.body);
-//   res.json({ message: 'Data received successfully' });
-// });
-
-app.get('/', (req, res) => {
-  res.send('Hello');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
